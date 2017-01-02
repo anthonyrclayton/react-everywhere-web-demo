dd9436bb60912321709ae75b37e0b708a2d81d89
Len Smith <len@barrison.com>
2017-01-01 20:37:58 -0800

    Fetch sessions from API

--- src/App.js
+++ src/App.js
@@ -2,37 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Session from './components/Session'
 import './App.css';
+const SESSIONS_URL = 'https://speakers.codemash.org/api/sessionsdata'
 
-const sessions = [{
-    "Id": 5445,
-    "SessionTime": "0001-01-01T00:00:00",
-    "SessionStartTime": "2017-01-10T08:00:00",
-    "SessionEndTime": "2017-01-10T12:00:00",
-    "Room": null,
-    "Rooms": [
-      "Indigo Bay"
-    ],
-    "Title": "7 Languages in 7 Hours",
-    "Abstract": "Hands-on experience in seven different languages, ranging from object-oriented to functional, from the cozy and familiar to the eye-squintingly terse and foreign. You'll work through a familiar kata in each of them, showing you how to approach an entirely new language. You'll learn about the strengths and weaknesses of each language out in the real world. You'll also see how each language's quirks can teach us ways to improve the code we write every day.\r\n\r\nYou may not come out of this pre-compiler an expert in all of these languages, but you'll have learned a lot about how to get started with a new one. You may even discover a new passion! At the very least, the next time a new language comes along, you'll have the tools you need to tackle it, and enough knowledge to help you push past the \"what is this syntax even doing\" barrier.\r\n\r\nLanguages: Ruby, Clojure, Haskell, Rust, Scala, Elixir, Go",
-    "SessionType": "Pre-Compiler",
-    "Tags": [
-      "Ruby/Rails",
-      "Testing",
-      "Functional Programming",
-      "Other"
-    ],
-    "Category": "Programming Principles",
-    "Speakers": [
-      {
-        "Id": "df1a9b69-1b7a-4224-bf28-2796a6cbed61",
-        "FirstName": "Amber",
-        "LastName": "Conville",
-        "GravatarUrl": "//www.gravatar.com/avatar/7c2a83b93fe0e5f2ed4c95c59367259c"
-      }
-    ]
-}]
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      sessions: [],
+      loading: true
+    }
+
+    fetch(SESSIONS_URL).then(r => r.json()).then((sessions) => {
+      this.setState({
+        sessions: sessions,
+        loading: false
+      })
+    })
+  }
+
+  renderSessions() {
+    return <h1>Loading...</h1>;
+
+    return this.state.sessions.map(session => <Session {...session} />)
+  }
+
   render() {
     return (
       <div className="App">
@@ -41,7 +36,7 @@ class App extends Component {
           <h2>CodeMash Schedule</h2>
         </div>
 
-        { sessions.each.map(session => <Session {...session} />) }
+        {this.renderSessions()}
       </div>
     );
   }
