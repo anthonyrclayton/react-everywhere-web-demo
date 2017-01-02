48342899eca2a757d48cd7b0c09ef3a5c2fc598c
Len Smith <len@barrison.com>
2017-01-01 22:08:30 -0800

    Stateless Functional Component

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react'
 
-class Speaker extends Component {
-  render() {
-    const { FirstName, LastName, GravatarUrl } = this.props
-
-    return(
-      <div className="speaker">
-        <div><img alt={FirstName} src={GravatarUrl}/></div>
-
-        <h9>{FirstName}&nbsp;{LastName}</h9>
-      </div>
-    )
-  }
-}
+const Speaker = ({ FirstName, LastName, GravatarUrl }) => (
+  <div className="speaker">
+    <div><img alt={FirstName} src={GravatarUrl}/></div>
+  
+    <h9>{FirstName}&nbsp;{LastName}</h9>
+  </div>
+)
 
 Speaker.propTypes =  {
   FirstName: React.PropTypes.string.isRequired,
