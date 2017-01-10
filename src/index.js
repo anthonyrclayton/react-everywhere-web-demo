9ac741b5965c6d35ccce5152a125fda5644df903
Len Smith <len@barrison.com>
2017-01-10 18:21:58 -0500

    hash history for static site

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import App from './App';
 import SpeakerDetails from './components/SpeakerDetails';
 import SessionList from './components/SessionList';
 import './index.css';
-import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
+import { Router, Route, IndexRedirect, hashHistory } from 'react-router'
 
 const router = (
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <IndexRedirect to="/sessions" />
-      <Route path="/sessions" component={SessionList}/>
-      <Route path="/speakers/:id" component={SpeakerDetails}/>
+      <IndexRedirect to="sessions" />
+      <Route path="sessions" component={SessionList}/>
+      <Route path="speakers/:id" component={SpeakerDetails}/>
     </Route>
   </Router>
 )
+
 ReactDOM.render(router,
   document.getElementById('root')
 );
